c92223ed7fc3389e8e949ff0dc7e47ee7142e407
Engr. Wurie <amaduwuriebah3761@gmail.com>
2024-12-24 13:03:12 +0000

    UPDATED: ADDED STUDENTS MODALS

--- unimak-lms/src/app/components/student-information/student-information.component.ts
+++ unimak-lms/src/app/components/student-information/student-information.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../../service/student.service';
 import { Student } from '../../models/Student';
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -10,61 +9,24 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule, ],
   templateUrl: './student-information.component.html',
-  standalone: true,
   styleUrls: ['./student-information.component.css']
 })
-export class StudentInformationComponent {
+export class StudentInformationComponent implements OnInit{
+
+  students: Student[] =[];
+
   isModalOpen = false;
   currentStep: number = 1;
-
-  student = {
-    photo: '',
-    phoneNumber: '',
-    email: '',
-    emergencyContactName: '',
-    emergencyContactNumber: '',
-    addressLine1: '',
-    addressLine2: '',
-    city: '',
-    country: '',
-    region: '',
-    district: '',
-    permanentAddressLine1: '',
-    permanentAddressLine2: '',
-    permanentCity: '',
-    permanentCountry: '',
-    permanentRegion: '',
-    permanentDistrict: '',
-    studentId: '',
-    firstName: '',
-    lastName: '',
-    gender: '',
-    birthDate: '',
-    department: '',
-    enrollmentDate: '',
-    status: '',
-    level: '',
-    studentType: '',
-  };
-
-
   sameAsCurrentAddress = false;
-
-  countries = ['Sierra Leone', 'USA', 'Canada', 'UK', 'Ghana', 'Nigeria', 'Liberia'];
-  regions = ['Eastern ', 'Northern', 'North Western', 'Southern ', 'Western Area', 'Western Urban'];
-  districts = ['Kailahun', 'Kenema', 'Kono', 'Bombali', 'Falaba', 'Koinadugu', 'Tonkolili'];
-  departments = ['Computer Science', 'Law', 'Public Health', 'Agriculture'];
 
   openModal() {
     this.isModalOpen = true;
     this.currentStep = 1;
   }
 
-  closeModal(event?: any) {
+  closeModal() {
     this.isModalOpen = false;
   }
-export class StudentInformationComponent implements OnInit {
-  students: Student[] = [];
 
   constructor(private studentService: StudentService) {}
 
@@ -82,11 +44,13 @@ export class StudentInformationComponent implements OnInit {
       }
     );
   }
+
   nextStep() {
     if (this.currentStep < 4) {
       this.currentStep++;
     }
   }
+
 
   previousStep() {
     if (this.currentStep > 1) {
@@ -94,20 +58,4 @@ export class StudentInformationComponent implements OnInit {
     }
   }
 
-  onFileSelected(event: any) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.student.photo = reader.result as string;
-      };
-      reader.readAsDataURL(file);
-    }
-  }
-
-
-  saveStudent() {
-    console.log('Student Data:', this.student);
-    this.closeModal();
-  }
 }
